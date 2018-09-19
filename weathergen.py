2a1a406defa2748772b12b625b0e0e1a319cd957
cosmologically <42528947+cosmologically@users.noreply.github.com>
2018-09-18 21:55:39 -0700

    weathergen update

--- weathergen.py
+++ weathergen.py
@@ -14,9 +14,18 @@ available = 0
 i = 0
 statuscat = "■■TODAY'S WEATHER:■■\n"
 biome = ["Sea of Nectar: ", "Sea of Tranquility: ", "Sea of Crises: ", "0"]
-descriptor = ["Light ", "Heavy ", "Intermittent ", "Hints of ", "Relentless ", "Overcast ", "Oppressive ", "Violent ", "Gentle ", "Pleasant ", "Mild ", "Aggressive ", "Impending ", "Thick ", "Thin ", "Sporadic ", "Strange ", "Unusual ", "Unreal ", "Hazy ", "Smoggy ", "Dusty ", "Questionable ", "Blissful ", "Unending ", "Sunny "]
-state = ["rain", "showers", "thunderstorms", "sunshine", "fog", "wind", "dryness", "humidity", "clouds", "snow", "drizzle", "smog", "sandstorms", "snowstorms", "hurricanes", "mist", "blizzard", "heatwaves", "hail", "rainbows", "flurries"]
 
+
+descriptor = []
+with open("...", "r") as f: # "..." needs to be full file path to descriptor file. ex: "C:\Users\Legion\Desktop\weatherbot\descriptorfile.txt"
+	for line in f.readlines():
+		descriptor = descriptor + line.strip("\n").split("\n")	
+f.close()	
+state = []
+with open("...", "r") as f: # "..." needs to be full file path to states file. ex: "C:\Users\Legion\Desktop\weatherbot\statesfile.txt"
+	for line in f.readlines():
+		state = state + line.strip().split("\n")	
+f.close()		
 #calculate how many biomes there are 	
 while not biome[i] == "0":
 	available = available + 1
@@ -28,9 +37,12 @@ CONSUMER_KEY = '...'
 CONSUMER_SECRET = '...'
 ACCESS_KEY = '...'
 ACCESS_SECRET = '...'
-auth = tweepy.0AuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
+
+user = api.me()
+print (user.name)
 
 	
 #iterate through list of biomes, concatenate randomized descriptions together
@@ -40,8 +52,8 @@ while i > 0:
 		rand_desc = random.choice(descriptor)
 		rand_state = random.choice(state)
 		statuscat = statuscat + biome[i] + rand_desc + rand_state + "\n" 
-	#print(statuscat) #for debugging on console
-	api.update_status(status=statuscat) #i don't actually know if this works yet as of 8/19
+	print(statuscat) #for debugging on console
+	api.update_status(statuscat)
 	statuscat = "■■TODAY'S WEATHER:■■\n"
 	time.sleep(86400) # tweet every 24 hours = 86400 sec. use 5 sec for console debugging
 	
